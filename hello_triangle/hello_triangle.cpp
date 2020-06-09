fe123e8a6fe14f9aa16dae06ac1c1e3deef208d2
jjtac <y1730900103@gmail.com>
2020-06-09 22:44:56 +0800

    add EBO

--- hello_triangle/hello_triangle.cpp
+++ hello_triangle/hello_triangle.cpp
@@ -131,20 +131,30 @@ int main(int argc, char const *argv[])
     glDeleteShader(fragmentShader);
 
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
+        // 第一个三角形
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, 0.5f, 0.0f,  // 左上角
+        -0.5f, -0.5f, 0.0f, // 左下角
+    };
 
-    GLuint VBO, VAO;
+    GLuint indices[] = {
+        0, 1, 3,
+        0, 2, 3};
+
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     //bind vertex array object && vertex buffer object
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
     // bind vertices data
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
     glEnableVertexAttribArray(0);
@@ -152,6 +162,9 @@ int main(int argc, char const *argv[])
     // unbind VAO && VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    // wireframe mode: only draw line
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // loop
     while (!glfwWindowShouldClose(window))
@@ -167,8 +180,10 @@ int main(int argc, char const *argv[])
 
         // draw triangle
         glUseProgram(shaderProgram);
+        // change to draw elements
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // Double Buffer swap
